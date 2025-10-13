cf07de50d8189ad808c607f231c1fcfeb7f5ce75
salomi-03 <22r21a3318@mlrit.ac.in>
2025-10-13 10:51:14 +0530

    Create Arithematic.js

---
+++ Arithematic.js
@@ -0,0 +1,12 @@
+let num1 =3
+let num2 =2
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = (num1 / num2).toFixed(2);	let modulus = num1 % num2;
+console.log(`Results:
+Addition: ${addition}
+Subtraction: ${subtraction}
+Multiplication: ${multiplication}
+Division: ${division}
+Modulus: ${modulus}`);
